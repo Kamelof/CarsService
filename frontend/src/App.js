cc04e48a334720c3e43583dbf4f146f9ee0fd18b
Leonardo1912 <80485115+Leonardo1912@users.noreply.github.com>
2021-11-17 22:14:11 +0200

    Transfer to React, update html and css

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Header from "./components/Header/header";
+import Main from "./components/Main/main";
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
